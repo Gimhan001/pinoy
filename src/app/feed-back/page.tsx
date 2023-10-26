ba6ee7b8a91e38c149e1adb7c0cf11c8310ff4c4
Gimhan001 <gimroxzy@gmail.com>
2023-10-26 16:06:34 +0530

    update google tag script 2

--- src/app/feed-back/page.tsx
+++ src/app/feed-back/page.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import Image from "next/image";
 import thankYou from "@/app/assets/images/thankYou.webp";
 import Script from "next/script";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Feed Back | Pinoy",
@@ -15,12 +16,13 @@ const inter = Inter({ subsets: ["latin"] });
 export default function FeedBack() {
   return (
     <main className={inter.className}>
-      <Script id="google-analytics">
+      <Head>
+      <script>
         {`
             gtag('event', 'conversion',{" "}
           {"send_to": "AW-11358860077/RQCSCILlxu8YEK3mqagq"});
             `}
-      </Script>
+      </script>
 
       <Script src="https://www.googletagmanager.com/gtag/js?id=AW-11358860077" />
       <Script id="google-analytics">
@@ -31,6 +33,9 @@ export default function FeedBack() {
           gtag('config', 'AW-11358860077');
         `}
       </Script>
+
+      </Head>
+      
       <Image src={thankYou} alt="Thank you" />
       <div className="container mx-auto p-4 grid justify-items-center border rounded-xl shadow-2xl ">
         <div className="group p-8  px-8">
