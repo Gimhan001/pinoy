1cb83371e104c00e7084da2dd2f2647b67c5760a
Gimhan001 <gimroxzy@gmail.com>
2023-10-26 16:09:33 +0530

    update google tag script 3

--- src/app/feed-back/page.tsx
+++ src/app/feed-back/page.tsx
@@ -15,38 +15,38 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function FeedBack() {
   return (
-    <main className={inter.className}>
+    <>
       <Head>
-      <script>
-        {`
+        <script>
+          {`
             gtag('event', 'conversion',{" "}
           {"send_to": "AW-11358860077/RQCSCILlxu8YEK3mqagq"});
             `}
-      </script>
+        </script>
 
-      <Script src="https://www.googletagmanager.com/gtag/js?id=AW-11358860077" />
-      <Script id="google-analytics">
-        {`
+        <script src="https://www.googletagmanager.com/gtag/js?id=AW-11358860077" />
+        <script>
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'AW-11358860077');
         `}
-      </Script>
-
+        </script>
       </Head>
-      
-      <Image src={thankYou} alt="Thank you" />
-      <div className="container mx-auto p-4 grid justify-items-center border rounded-xl shadow-2xl ">
-        <div className="group p-8  px-8">
-          <h2 className="text-center text-3xl font-bold uppercase">
-            Thank You...!!!
-          </h2>
-          <p className="text-center text-base mt-8">
-            We will contact you soon..
-          </p>
+      <main className={inter.className}>
+        <Image src={thankYou} alt="Thank you" />
+        <div className="container mx-auto p-4 grid justify-items-center border rounded-xl shadow-2xl ">
+          <div className="group p-8  px-8">
+            <h2 className="text-center text-3xl font-bold uppercase">
+              Thank You...!!!
+            </h2>
+            <p className="text-center text-base mt-8">
+              We will contact you soon..
+            </p>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
